8010077d47032428a8fee6f3263e42420dc0df9d
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-14 16:40:03 +0300

    Proje.kt

---
+++ Proje.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+fun main (args : Array<String>)
+{
+    var num1:Float
+    num1 = 111.572666F
+    var num2:Float = 40.714535F
+    var FloatMakximum:Float = Float.MAX_VALUE
+    var FloatMinimum:Float = Float.MIN_VALUE
+
+    println("num1="+num1)
+    println("num2="+num2)
+    println("FloatMaximum="+FloatMakximum)
+    println("FloatMinimum="+FloatMinimum)
+}
